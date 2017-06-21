f5efa22ddbd39b5e4a458ec8ec2c3bfc417300c0
jaswanth <jaswanth.ruby@gmail.com>
2017-06-21 21:12:31 +0000

    Exit

--- MusicChimpanzies/config/routes.rb
+++ MusicChimpanzies/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
- root 'application#hello'
+
+ root 'pages#home'
+ get  '/about', to: 'pages#about'
+ get  '/help', to: 'pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
